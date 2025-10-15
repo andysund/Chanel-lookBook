a06c7ddc8f37124e63b29e06f8282a5abfdddde7
andysund <tube88771@gmail.com>
2025-10-15 14:04:35 +0200

    feat: Font Awesome local install, header icons, styles updates

--- Chanel/src/components/DownFlow.jsx
+++ Chanel/src/components/DownFlow.jsx
@@ -1,7 +1,27 @@
 import './DownFlow.css'
+import backOne from '../assets/carousel/back-one.webp'
+import frontFive from '../assets/carousel/front-five.webp'
+import lowerOne from '../assets/carousel/lower-two.webp'
+import sideTwo from '../assets/carousel/side-two.webp'
 function DownFlow() {
   return (
-    <div className="downflow">DownFlow</div>
+    <>
+    <div className="downflow-background">
+       <div className="img-background">
+        <img src={backOne} alt="Back-one"/>
+       </div>
+       <div className="img-background">
+        <img src={frontFive} alt="front-five"/>
+       </div>
+       <div className="img-background">
+        <img src={lowerOne} alt="lower-one"/>
+       </div>
+       <div className="img-background">
+        <img src={sideTwo} alt="side-two"/>
+       </div>
+    </div>
+
+    </>
   )
 }
 
